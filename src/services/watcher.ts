40d0180aae95c91b27c9505818ed447e13582453
JamesTeague <james@teague.dev>
2023-06-01 10:21:19 -0500

    fix: compare dates correctly

--- src/services/watcher.ts
+++ src/services/watcher.ts
@@ -16,57 +16,57 @@ export const createWatcher = (
 
 const watch =
   (client: any, publisher: Publisher, logger: Stoolie) =>
-  async ({ rssFeed, frequencyMs }: WatchRequest): Promise<Subscription> => {
-    let lastReadDate: Date;
-    let showLink: string;
-    const log = logger;
+    async ({ rssFeed, frequencyMs }: WatchRequest): Promise<Subscription> => {
+      let lastReadDate: Date;
+      let showLink: string;
+      const log = logger;
 
-    const exit = new Subject();
-    ['SIGINT', 'SIGTERM', 'SIGHUP'].forEach((signal) => {
-      process.on(signal, () => exit.next(true));
-    });
-
-    return interval(frequencyMs)
-      .pipe(
-        RxOp.takeUntil(exit),
-        RxOp.mergeMap(() => getMostRecentPostDate(client)),
-        RxOp.tap((mostRecentPostDate) => {
-          lastReadDate = mostRecentPostDate;
-        }),
-        RxOp.tap(() => {
-          log.withFields({ lastReadDate }).info('Checking for new posts...');
-        }),
-        RxOp.mergeMap(() => parse(rssFeed)),
-        RxOp.tap(({ link }) => {
-          showLink = link;
-        }),
-        RxOp.map(({ items }: Feed) => items),
-        RxOp.mergeMap((items) => items),
-        RxOp.filter(
-          ({ published }) =>
-            new Date(published).toISOString() > lastReadDate.toISOString()
-        ),
-        RxOp.tap(({ title, published }) =>
-          log
-            .withFields({ title, published: new Date(published).toISOString() })
-            .info('Publishing Post.')
-        ),
-        RxOp.mergeMap((item: PodcastItem) =>
-          publisher.publish({ showLink, item, tags: ['Podcast'] })
-        )
-      )
-      .subscribe({
-        next() {},
-        error(error: Error) {
-          log.withError(error).error('Fatal application error.');
-        },
-        complete() {
-          log.info('Application exited normally.');
-        },
+      const exit = new Subject();
+      ['SIGINT', 'SIGTERM', 'SIGHUP'].forEach((signal) => {
+        process.on(signal, () => exit.next(true));
       });
-  };
+
+      return interval(frequencyMs)
+        .pipe(
+          RxOp.takeUntil(exit),
+          RxOp.mergeMap(() => getMostRecentPostDate(client)),
+          RxOp.tap((mostRecentPostDate) => {
+            lastReadDate = mostRecentPostDate;
+          }),
+          RxOp.tap(() => {
+            log.withFields({ lastReadDate }).info('Checking for new posts...');
+          }),
+          RxOp.mergeMap(() => parse(rssFeed)),
+          RxOp.tap(({ link }) => {
+            showLink = link;
+          }),
+          RxOp.map(({ items }: Feed) => items),
+          RxOp.mergeMap((items) => items),
+          RxOp.filter(
+            ({ published }) =>
+              new Date(published).getTime() > lastReadDate.getTime()
+          ),
+          RxOp.tap(({ title, published }) =>
+            log
+              .withFields({ title, published: new Date(published).toISOString() })
+              .info('Publishing Post.')
+          ),
+          RxOp.mergeMap((item: PodcastItem) =>
+            publisher.publish({ showLink, item, tags: ['Podcast'] })
+          )
+        )
+        .subscribe({
+          next() { },
+          error(error: Error) {
+            log.withError(error).error('Fatal application error.');
+          },
+          complete() {
+            log.info('Application exited normally.');
+          },
+        });
+    };
 
 const getMostRecentPostDate = async (client: any): Promise<Date> => {
-  const mostRecentPost = (await client.posts.browse({ limit: 1 }))[0];
+  const mostRecentPost = (await client.posts.browse({ limit: 1, sort: 'published_at DESC', filter: 'tag:podcast' }))[0];
   return new Date(mostRecentPost.published_at);
 };
